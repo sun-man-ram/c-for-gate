5afec53744b43cc248bcfa31698602509a001e9f
sun-man-ram <manikantasunkara2653@gmail.com>
2024-06-17 22:25:09 +0530

    lec 3 not completed

---
+++ lec3.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// #incude<iostream>
+// using namespace std;
+int main(){
+    char x='a';
+    if(x||12){
+        // short cirvaluationcuit e
+        printf("hello");
+    }
+
+    return 0;
+}
