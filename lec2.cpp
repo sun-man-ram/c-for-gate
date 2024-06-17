f49420a4e2cc41b9b3401b8bdf760d0fc801e74c
sun-man-ram <manikantasunkara2653@gmail.com>
2024-06-17 21:37:10 +0530

    lec 2 is updated

--- lec2.cpp
+++ lec2.cpp
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main(){
-
-
-
+  //operators
+  int a=3;
+//   2=a;
+//   (2+3)=5;
+  printf("%d\n",10+20);
+   printf("%d\n",12%7);
+   printf("%d\n",(-(12%7)));
+   printf("%f\n",10.0/2.0);
+   printf("%f\n",5.0/2);
+//    printf("%f",(30.0/2%6+4));
+int y=9;
+printf("%d",y);
     return 0;
 }
