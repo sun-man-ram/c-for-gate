b3d8ad489a26728abc616b07d540940ab65da5c6
sun-man-ram <manikantasunkara2653@gmail.com>
2024-06-17 21:14:10 +0530

    lec 1 is added

---
+++ lec1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+int main(){
+  int a=10;
+  printf("%d\n",a);
+  float a2=10.5;
+  printf("%f\n",a2);
+ unsigned short int a3=-3276;
+ printf("%d\n",a3);
+ printf("%d\n",sizeof(short int));
+ printf("%d\n",a);
+ char c='c';
+ printf("%d\n",c);
+  signed short int a4=32768;
+  printf("%d\n",a4);
+  int read;
+  scanf("%d",&read);
+    return 0;
+}
